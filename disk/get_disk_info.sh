33a03cfb956ab79b888bbf8962bb9ff501255088
Lisbeth-ly <38647891+Lisbeth-ly@users.noreply.github.com>
2019-01-15 11:42:06 +0800

    Create get_disk_info.sh

---
+++ disk/get_disk_info.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+  
+diskarray=(`cat /proc/diskstats |grep -E "\bsd[a-z]\b|\bxvd[a-z]\b"| grep -i "\b$1\b"| awk '{print $3}'| sort| uniq   2>/dev/null`)
+length=${#diskarray[@]}  
+printf "{\n"  
+printf '\t'"\"data\":["  
+for ((i=0;i<$length;i++))  
+  do  
+         printf '\n\t\t{'  
+         printf "\"{#DISK_NAME}\":\"${diskarray[$i]}\"}"  
+         if [ $i -lt $[$length-1] ];then  
+                 printf ','  
+         fi  
+ done  
+ printf "\n\t]\n"  
+ printf "}\n"  
